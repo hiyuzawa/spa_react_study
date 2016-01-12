9f0806dc889186cedef4a8b2a3ecf095cc92562d
Hideto Yuzawa <hyuzawa@yahoo-corp.jp>
2016-01-12 22:29:01 +0900

    CHECK POINT 4

--- js/app.js
+++ js/app.js
@@ -1,4 +1,13 @@
+var _ = require("lodash");
+var my_module1 = require("./my_module1.js");
+
 $(function(){
     console.log("Hello from javascript");
     $("#title").text("Hello from jquery");
+
+    var array = [1,3,5,7,9];
+    _.each(array, function(element, index, array) {
+        console.log(index + ":" + element + ":" + my_module1.add(element,index));
+    });
 })
+

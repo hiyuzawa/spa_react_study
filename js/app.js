5b617ce7202973c40fd6700869a50d691ce77f3b
Hideto Yuzawa <hyuzawa@yahoo-corp.jp>
2016-01-12 22:47:11 +0900

    CHECK POINT 7

--- js/app.js
+++ js/app.js
@@ -1,5 +1,7 @@
 var _ = require("lodash");
 var my_module1 = require("./my_module1.js");
+var React = require('react');
+var ReactDOM = require('react-dom');
 
 import MyModule2 from './my_module2';
 
@@ -14,5 +16,20 @@ $(function(){
 
     const my_module2 = new MyModule2("hiyuzawa");
     console.log(my_module2.sayHello());
+
+    var Hello = React.createClass({
+        render: function() {
+            return (
+                <div className="Hello">
+                    Hello React World!! by {this.props.name}
+                </div>
+            );
+        }
+    })
+
+    ReactDOM.render(
+        <Hello name="hyuzawa"/>,
+        document.getElementById('content')
+    );
 })
 

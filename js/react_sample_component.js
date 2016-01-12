6253d76d59a4b9ba0939d3c8215fceb3e4e1429e
Hideto Yuzawa <hyuzawa@yahoo-corp.jp>
2016-01-12 23:02:25 +0900

    CHECK POINT 10

--- js/react_sample_component.js
+++ js/react_sample_component.js
@@ -1,13 +1,15 @@
 var React = require("react");
+var ReactDOM = require("react-dom");
 
 var Sample = React.createClass({
     handleNameButtonClick: function() {
-        this.props.chageName("Hideto Yuzawa");
+        this.props.chageName(ReactDOM.findDOMNode(this.refs.name_text).value);
     },
     render() {
         return (
             <div className="sample">
                 <p>Hello!! from Sample Component!! {this.props.date} by {this.props.name}</p>
+                <input type="text" ref="name_text" />
                 <input type="button" onClick={this.handleNameButtonClick} value="chage me" />
             </div>
             );
